e7f9d67e445e9614801d5d9e610fcf13b150056c
Duygu Algan <duygu.algan.esk26@gmail.com>
2023-02-15 21:50:11 +0300

    edit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+		header.style.backgroundColor = '#c32317';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
